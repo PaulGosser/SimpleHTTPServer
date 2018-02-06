7085b4e31005d17c230f5f404e3cd009fbec60a8
Paul Gosser <paumgoss@ut.utm.edu>
2018-02-05 18:27:33 -0600

    Created Exception.

--- java/HTTPServer.java
+++ java/HTTPServer.java
@@ -1,1 +1,11 @@
 import java.net.ServerSocket;
+
+public class HTTPServer{
+    public static void main(String[] args) throws Exception {
+	final ServerSocket server = new ServerSocket(8080);
+	System.out.println("Listening for 8080.");
+	while (true){
+
+	}
+    }
+}
