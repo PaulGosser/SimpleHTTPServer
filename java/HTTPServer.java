bbe58d2b90ff22b9d2006fb43becebd3ae4259a9
Paul Gosser <paumgoss@ut.utm.edu>
2018-02-05 21:04:29 -0600

    Posts from server.

--- java/HTTPServer.java
+++ java/HTTPServer.java
@@ -10,15 +10,20 @@ public class HTTPServer{
 	final ServerSocket server = new ServerSocket(8080);
 	System.out.println("Listening for 8080.");
 	while (true){
-	    final Socket client = server.accept();
+	    try(final Socket client = server.accept()){
 
-	    // 1. Read HTTP request from the client socket
-	    InputStreamReader isReader = new InputStreamReader(client.getInputStream());
-	    BufferedReader bReader = new BufferedReader(isReader);
-	    String line = bReader.readLine();
-	    while (!line.isEmpty()) {
-		System.out.println(line);
-		line = bReader.readLine();
+		// 1. Read HTTP request from the client socket
+		InputStreamReader isReader = new InputStreamReader(client.getInputStream());
+		BufferedReader bReader = new BufferedReader(isReader);
+		String line = bReader.readLine();
+		while (!line.isEmpty()) {
+		    System.out.println(line);
+		    line = bReader.readLine();
+		}
+
+		Date today = new Date();
+		String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+		client.getOutputStream().write(httpResponse.getBytes("UTF-8"));
 	    }
 
 	    // 2. Prepare an HTTP response
